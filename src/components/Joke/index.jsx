56b7a1b5801a7baf3c41e96773070efa9ffa3034
unknown <k.jozifova@gmail.com>
2023-11-25 18:17:02 +0100

    ukol č. 16 fin

--- src/components/Joke/index.jsx
+++ src/components/Joke/index.jsx
@@ -1,1 +1,19 @@
-export const Joke = ({ avatar, name, text, likes, dislikes }) => {};
+export const Joke = ({ avatar, name, text, likes, dislikes }) => {
+  return (
+    <div>
+      <img className="user-avatar" src={avatar} />
+      <p className="user-name">{name}</p>
+      <p className="joke__text">{text}</p>
+      <div className="joke__likes">
+        <button id="btn-up" className="btn-like btn-like--up">
+          {likes}
+        </button>
+        <span id="likes-up" className="likes-count likes-count--up"></span>
+        <button id="btn-down" className="btn-like btn-like--down">
+          {dislikes}
+        </button>
+        <span id="likes-down" className="likes-count likes-count--down"></span>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,51 +1,34 @@
 import './style.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Joke } from '../../components/Joke';
 
 export const HomePage = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
+  const [jokes, setJokes] = useState(null);
+
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/Czechitas-podklady-WEB/daweb-test/deploy/jokes.json',
+      );
+      const data = await response.json();
+      setJokes(data);
+    };
+    fetchJokes();
+  }, []);
 
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user03.png"
-            />
-            <p className="user-name">Oracimaru</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => {
-              setUpLikes(upLikes + 1);
-            }}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {upLikes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => {
-              setDownLikes(downLikes + 1);
-            }}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {downLikes}
-          </span>
-        </div>
-      </div>
+      {jokes &&
+        jokes.map((joke, name) => (
+          <Joke
+            key={name}
+            avatar={joke.avatar}
+            name={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        ))}
     </div>
   );
 };
